c39ca15d2699a54e03749936233ae61378ae4fb3
user <user@users-MacBook-Pro.local>
2019-04-02 14:00:47 -0700

    updated data

--- api/post/index.js
+++ api/post/index.js
@@ -3,32 +3,31 @@ const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
 const cors = require('@koa/cors');
  const pool = require('../../db/db')
-const { twiml: { MessagingResponse } } = require('twilio')
+ const MessagingResponse = require('twilio').twiml.MessagingResponse
 const app = new Koa()
 app.use(bodyParser())
 app.use(cors())
 
+
 app.use(async ctx => {
-  const data = await ctx.request.body
-  const item = await createPost(data.todoItem)
+  ctx.type = 'text/xml';
+const data = await ctx.request.body
+const item = await createPost(data.todoItem)
+  ctx.body = item;
   const twiml = new MessagingResponse();
-  twiml.message(`${req.body}`)
-  ctx.body = `new todoList created, todoItem ${item.inserttodoItem}`
-  ctx.type='text/xml';
+ twiml.message('New todoList Item created');
+ return twiml.toString();
 })
 
 async function createPost(todoItem) {
-  try {
-    const itemData = await pool.query(`
-      INSERT INTO list(todoItem) 
-      VALUES ("${todoItem}");
-    `)
-    return itemData
-  } catch (error) {
-    console.log(error)
+    try {
+      const itemData = await pool.query(`INSERT INTO list(todoItem) VALUES ('${todoItem}');`)
+      return itemData
+    } catch (error) {
+      console.log(error)
+    }
   }
-}
-module.exports = app.callback()
 
+  module.exports = app.callback()
 
 
